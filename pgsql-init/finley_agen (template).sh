4a88447c123b6b1bf28a8d13c711309d32b82902
Peter Yahia <peteryahia12@gmail.com>
2021-09-07 00:59:46 -0400

    template

--- pgsql-init/finley_agen (template).sh
+++ pgsql-init/finley_agen (template).sh
@@ -4,18 +4,26 @@ if [ -z ${pgsqlpwd+x} ]; then echo export pgsqlpwd=\"$(head -c 16  /dev/random |
 if [ -z ${pgsqluid+x} ]; then echo export pgsqluid=\"finleysa\" >> ~/.bashrc; else echo "var is set"; fi 
 if [ -z ${pgsqlhost+x} ]; then echo export pgsqlhost=\"{{EC2 PUBLIC IP}}\" >> ~/.bashrc; else echo "var is set"; fi
 
+
 source ~/.bashrc
 
-ssh -i /opt/airflow/ssh/finley.pem -y  ubuntu@$pgsqlhost "sudo echo export pgsqlpwd=$pgsqlpwd >> /opt/.pgsqlcreds"
-ssh -i /opt/airflow/ssh/finley.pem -y  ubuntu@$pgsqlhost "sudo echo export pgsqluid=$pgsqluid >> /opt/.pgsqlcreds"
+su - airflow -c "echo export pgsqlpwd=$pgsqlpwd"
+su - airflow -c "echo export pgsqluid=$pgsqluid"
+su - airflow -c "echo export pgsqlhost=$pgsqlhost"
 
+ssh -i /opt/airflow/ssh/finley.pem ubuntu@$pgsqlhost "echo chmod 777 /opt/ "
+
+ssh -i /opt/airflow/ssh/finley.pem  ubuntu@$pgsqlhost <<EOF 
+echo chmod 777 /opt/ 
+echo export pgsqlpwd=$pgsqlpwd >> /opt/.pgsqlcreds
+echo export pgsqluid=$pgsqluid >> /opt/.pgsqlcreds
 sudo su postgres
 source /opt/.pgsqlcreds
+psql -c "create role $pgsqluid with login password '$pgsqlpwd'"
+EOF
 
-createuser --interactive --pwprompt
-$pgsqluid
-$pgsqlpwd
-$pgsqlpwd
-yes
+scp -i /opt/airflow/ssh/finley.pem /opt/airflow/pgsql-init/sql/finley_init.sql ubuntu@$pgsqlhost:/opt/
 
-sudo rm /opt/.pgsqlcreds
+
+
+
