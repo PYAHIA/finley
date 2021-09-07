fc10265aa9f8e5f9c4bc5b96240f7515191752fe
Peter Yahia <peteryahia12@gmail.com>
2021-09-06 23:45:00 -0400

    better-parsing

--- modules/parse_code.py
+++ modules/parse_code.py
@@ -16,12 +16,16 @@ class Procedure():
         self.parse_sources()
 
     def parse_targets(self):
-        pattern = re.compile(r'INSERT\s+INTO\s+(.+\..+)')
+        pattern = re.compile(r'INSERT\s+(?:\/\*s*\+s*DIRECTs*\*\/\s+)?INTO\s+(.+\..+)')
         matches = re.findall(pattern, self.code)
         pattern2 = re.compile('CREATE\s+TABLE\s+(?:IF\s+NOT\s+EXISTS\s+)?(.+\..+)\s+AS')
         matches2 = re.findall(pattern2, self.code)
-        targets = list(set(matches+matches2))
-
+        pattern3 = re.compile("SWAP_PARTITIONS_BETWEEN_TABLES\('(.+)',.+,.+,'(.+)'\)")
+        matches3 = re.findall(pattern3,self.code)
+        if matches3:
+            matches3 = list(matches3[0])
+        targets = list(set(matches+matches2+matches3))
+        
         self.targets = [target for target in targets if target.count('.') == 1]
         
     def parse_sources(self):
@@ -30,13 +34,12 @@ class Procedure():
         pattern2 = re.compile(r'JOIN\s+(\S+)\s+')
         matches2 = re.findall(pattern2, self.code)
         sources = list(set(matches + matches2))
-        
         self.sources = [source for source in sources if source.count('.') == 1]
 
     @property
     def root_path(self):
         if os.name == 'nt':
-            return r"C:/Users/pyahia/git/airflow-docker/repos"
+            return r"C:/Users/pyahia/git/finley/repos"
         else:
             return r"opt/airflow/repos"
 
--- repos/dvd-dw/film_pre.sql
+++ repos/dvd-dw/film_pre.sql
@@ -2,3 +2,10 @@ DROP TABLE IF EXISTS dw.film_pre CASCADE;
 CREATE TABLE dw.film_pre AS
 SELECT * FROM public.film
 ;
+
+DROP TABLE IF EXISTS dw.film_pre2;
+CREATE TABLE dw.film_pre2 LIKE dw.film_pre INCLUDING PROJECTIONS;
+INSERT /*+DIRECT*/ INTO dw.film_pre2;
+SELECT * FROM public.film
+;
+
